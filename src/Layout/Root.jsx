45a2aec319dd92c6a6d9b9ac373f71038ad9871c
Moshiur Rahman <rifatmoshiur00@gmail.com>
2024-05-07 00:30:24 +0600

    addproduct page form and upadte form added with responsive design. footer added on root for all the pages along with header

--- src/Layout/Root.jsx
+++ src/Layout/Root.jsx
@@ -1,5 +1,6 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../Components/Navbar/Navbar";
+import Footer from "../Components/Footer/Footer";
 
 
 const Root = () => {
@@ -7,6 +8,7 @@ const Root = () => {
         <div className='font-teacher max-w-screen-xl mx-auto p-5 md:p-10'>
             <Navbar/>
             <Outlet/>
+            <Footer/>
         </div>
     );
 };
--- src/Pages/AddItems/AddItems.jsx
+++ src/Pages/AddItems/AddItems.jsx
@@ -1,9 +1,64 @@
 
 const AddItems = () => {
+    const handleAddProduct = e =>{
+        e.preventDefault()
+        const form = e.target
+        const url = form.url?.value
+        const price = form.price?.value
+        const rating = form.rating?.value
+        const description = form.description?.value
+        const category = form.category?.value
+        console.log(form,url,price,rating,description,category);
+    }
     return (
-        <div>
-            
-        </div>
+        <section className="bg-white dark:bg-gray-900">
+            <div className="py-8 px-4 mx-auto max-w-2xl lg:py-16">
+                <h2 className="mb-4 text-xl font-bold text-gray-900 dark:text-white">Add a new product</h2>
+                <form onSubmit={handleAddProduct}>
+                    <div className="grid gap-4 sm:grid-cols-2 sm:gap-6">
+                        <div className="sm:col-span-2">
+                            <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Image url</label>
+                            <input type="url" name="url" id="name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="image url" required=""></input>
+                        </div>
+                        <div className="sm:col-span-2">
+                            <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Product Name</label>
+                            <input type="text" name="name" id="name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Type product name" required=""></input>
+                        </div>
+                        <div className="w-full">
+                            <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Brand</label>
+                            <input type="text" name="brand" id="brand" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Product brand" required=""></input>
+                        </div>
+                        <div className="w-full">
+                            <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Price</label>
+                            <input type="number" name="price" id="price" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="$299" required=""></input>
+                        </div>
+                        <div>
+                            <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Category</label>
+                            <select name="category" id="category" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
+                                <option selected="">Select category</option>
+                                <option value="Shirt">Shirt</option>
+                                <option value="Pant">Pant</option>
+                                <option value="T-Shirt">T-Shirt</option>
+                                <option value="Shoes">Shoes</option>
+                                <option value="Sunglass">Sunglass</option>
+                                <option value="Wallet">Wallet</option>
+                            </select>
+                        </div>
+                        <div>
+                            <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Rating</label>
+                            <input type="number" name="rating" id="item-weight" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="5" required="" min="1" max="5"></input>
+                        </div>
+                        <div className="sm:col-span-2">
+                            <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Description</label>
+                            <textarea name="description" id="description" rows="5" className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Your description here"></textarea>
+                        </div>
+                    </div>
+                    <button type="submit" className="inline-flex items-center px-5 py-2.5 mt-4 sm:mt-6 text-sm font-medium text-center text-white bg-primary-700 rounded-lg focus:ring-4 focus:ring-primary-200 dark:focus:ring-primary-900 hover:bg-primary-800">
+                        Add product
+                    </button>
+                </form>
+            </div>
+        </section>
     );
 };
 
